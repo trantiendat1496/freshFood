51d9c3a345df5178502fc3c02de74a641af22570
Tien Dat <datttbhaf180235@fpt.edu.vn>
2020-02-19 15:20:08 +0700

    edit file conection

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ class DB{
 	public static function connection(){
 		if(!isset(self::$db)){
 			try{
-				self::$db = new PDO('mysql:host=34.207.226.216;dbname=freshFood', 'f_user', '123@123a');
+				self::$db = new PDO('mysql:host=ec2-34-207-226-216.compute-1.amazonaws.com;dbname=freshFood', 'f_user', '123@123a');
       			self::$db->exec("SET NAMES 'utf8'");
 			}catch(PDOException $ex){
 				echo "Lỗi trong quá trình kết nối CSDL: ". $ex->getMessage();
